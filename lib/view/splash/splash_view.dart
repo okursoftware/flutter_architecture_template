f0fdc1a0fd52d13b84ad8f7b66123c7765544ef9
Muhammed OKUR <okurokur2014@gmail.com>
2020-06-12 07:53:08 +0300

    Responsive helper init

--- lib/view/splash/splash_view.dart
+++ lib/view/splash/splash_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_architecture/utils/responsive_helper.dart';
 import 'package:flutter_architecture/view/splash/splash_viewmodel.dart';
 import 'package:stacked/stacked.dart';
 
@@ -6,14 +7,18 @@ class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<SplashViewModel>.reactive(
-        builder: (context, model, child) => Scaffold(
-              appBar: AppBar(
-                title: Text("Splash Screen"),
-              ),
-              body: Center(
-                child: model.isBusy ? CircularProgressIndicator() : Container(),
-              ),
+        builder: (context, model, child) {
+          //this adjustment should be made on the first screen opened
+          SizeConfig().init(context);
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Splash Screen"),
             ),
+            body: Center(
+              child: model.isBusy ? CircularProgressIndicator() : Container(),
+            ),
+          );
+        },
         viewModelBuilder: () => SplashViewModel());
   }
 }
